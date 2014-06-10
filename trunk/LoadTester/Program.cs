9db90547e1ad19f787c939a81c71652c97efef6a
Yanal-Yves <yanal_yves_fargialla@hotmail.com>
2014-06-10 01:37:25 +0000

    R#

--- trunk/LoadTester/Program.cs
+++ trunk/LoadTester/Program.cs
@@ -40,19 +40,19 @@ namespace LoadTester
         const int NbThread = 20;
 
         /// <summary>
+        /// Tables of cookies. Each thread has one cookie. Used only if PreserveCookie is true
+        /// </summary>
+        static readonly CookieCollection[] Cookies = new CookieCollection[NbThread];
+
+        /// <summary>
         /// Compte le nombre d'itération de tous les threads 
         /// </summary>
-        static int nbRequest = 0;
+        static int nbRequest;
 
         /// <summary>
         /// Compte le nombre d'itération de tous les threads pour le test sur la session IIS
         /// </summary>
-        static int nbRequestSessAsp = 0;
-
-        /// <summary>
-        /// Tables of cookies. Each thread has one cookie. Used only if PreserveCookie is true
-        /// </summary>
-        static CookieCollection[] cookies = new CookieCollection[NbThread];
+        static int nbRequestSessAsp;
 
         /// <summary>
         /// Fait le même job que DoWork plus un comptage d'itération avec affichage
@@ -64,7 +64,9 @@ namespace LoadTester
                 Thread.Sleep(10000);
                 Console.WriteLine("Number of HTTP requests handled by AspSessionService:  " + nbRequest);
             }
+// ReSharper disable FunctionNeverReturns
         }
+// ReSharper restore FunctionNeverReturns
 
         /// <summary>
         /// Appel la page de test du service de session avec erreur 500 et sans erreur
@@ -72,20 +74,22 @@ namespace LoadTester
         /// <param name="threadNumber">the number of the thread</param>
         static void CallTest(object threadNumber)
         {
-            int thread_number = (int)threadNumber;
+            var threadNumberInt = (int)threadNumber;
             for (;;)
             {
                 string url = "http://localhost/SessionService/Test.asp";
-                RequestUrl(url, true, false, thread_number);
+                RequestUrl(url, true, false, threadNumberInt);
                 url += "?Error=1";
-                RequestUrl(url, false, false, thread_number);
+                RequestUrl(url, false, false, threadNumberInt);
                 url = "http://localhost/SessionService/TestNoSessionAccess.asp";
-                RequestUrl(url, true, false, thread_number);
+                RequestUrl(url, true, false, threadNumberInt);
                 url = "http://localhost/SessionService/TestReload.asp";
-                RequestUrl(url, true, false, thread_number);
+                RequestUrl(url, true, false, threadNumberInt);
                 Interlocked.Add(ref nbRequest, 1);
             }
+// ReSharper disable FunctionNeverReturns
         }
+// ReSharper restore FunctionNeverReturns
 
         /// <summary>
         /// Fait le même job que DoWork plus un comptage d'itération avec affichage
@@ -97,7 +101,9 @@ namespace LoadTester
                 Thread.Sleep(10000);
                 Console.WriteLine("IIS Session : " + nbRequestSessAsp);
             }
+// ReSharper disable FunctionNeverReturns
         }
+// ReSharper restore FunctionNeverReturns
 
         /// <summary>
         /// Appel la page de test du service de session avec erreur 500 et sans erreur.
@@ -105,16 +111,18 @@ namespace LoadTester
         /// <param name="threadNumber">the number of the thread</param>
         static void CallTestIisSession(object threadNumber)
         {
-            int thread_number = (int)threadNumber;
+            var threadNumberInt = (int)threadNumber;
             for (;;)
             {
                 string url = "http://localhost/SessionService/TestIisSession.asp";
-                RequestUrl(url, true, false, thread_number);
+                RequestUrl(url, true, false, threadNumberInt);
                 url += "?Error=1";
-                RequestUrl(url, false, false, thread_number);
+                RequestUrl(url, false, false, threadNumberInt);
                 Interlocked.Add(ref nbRequestSessAsp, 1);
             }
+// ReSharper disable FunctionNeverReturns
         }
+// ReSharper restore FunctionNeverReturns
 
         /// <summary>
         /// Appel la page de test du service de session avec erreur 500 et sans erreur
@@ -122,14 +130,16 @@ namespace LoadTester
         /// <param name="threadNumber">the number of the thread</param>
         static void CallTestScriptingDictionary(object threadNumber)
         {
-            int thread_number = (int)threadNumber;
+            var threadNumberInt = (int)threadNumber;
             for (;;)
             {
-                string url = "http://localhost/SessionService/TestScriptingDictionary.asp";
-                RequestUrl(url, true, false, thread_number);
+                const string Url = "http://localhost/SessionService/TestScriptingDictionary.asp";
+                RequestUrl(Url, true, false, threadNumberInt);
                 Interlocked.Add(ref nbRequest, 1);
             }
+// ReSharper disable FunctionNeverReturns
         }
+// ReSharper restore FunctionNeverReturns
 
         /// <summary>
         /// Request the Session Service test page
@@ -140,15 +150,16 @@ namespace LoadTester
         /// <param name="threadNumber">the number of the current thread</param>
         private static void RequestUrl(string url, bool stopIfInternalError, bool displayInfo, int threadNumber)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            string result;
+            var request = (HttpWebRequest)WebRequest.Create(url);
             request.CookieContainer = new CookieContainer();
 
+// ReSharper disable ConditionIsAlwaysTrueOrFalse
             if (PreserveCookie)
+// ReSharper restore ConditionIsAlwaysTrueOrFalse
             {
-                if (Program.cookies[threadNumber] != null && Program.cookies[threadNumber].Count > 0)
+                if (Cookies[threadNumber] != null && Cookies[threadNumber].Count > 0)
                 { // Set the cookie
-                    request.CookieContainer.Add(Program.cookies[threadNumber]);
+                    request.CookieContainer.Add(Cookies[threadNumber]);
                 }
             }
 
@@ -166,12 +177,14 @@ namespace LoadTester
                         throw new Exception("Code de retour != 200");
                     }
 
-                    StreamReader sr = new StreamReader(response.GetResponseStream());
-                    result = sr.ReadToEnd();
+// ReSharper disable AssignNullToNotNullAttribute
+                    var sr = new StreamReader(response.GetResponseStream());
+// ReSharper restore AssignNullToNotNullAttribute
+                    sr.ReadToEnd();
 
                     if (response.Cookies.Count > 0)
                     {
-                        Program.cookies[threadNumber] = response.Cookies;
+                        Cookies[threadNumber] = response.Cookies;
                     }
                 }
 
@@ -186,9 +199,11 @@ namespace LoadTester
                 {
                     Console.WriteLine(ex.Response.ToString());
                     Stream s = ex.Response.GetResponseStream();
-                    StreamReader sr = new StreamReader(s);
-                    var str_response = sr.ReadToEnd();
-                    Console.WriteLine(str_response);
+// ReSharper disable AssignNullToNotNullAttribute
+                    var sr = new StreamReader(s);
+// ReSharper restore AssignNullToNotNullAttribute
+                    var strResponse = sr.ReadToEnd();
+                    Console.WriteLine(strResponse);
                     throw new Exception("500: Interal Error", ex);
                 }
 
@@ -202,21 +217,22 @@ namespace LoadTester
         /// <summary>
         /// Test entry point
         /// </summary>
-        /// <param name="args">Application argument : none</param>
-        static void Main(string[] args)
+        static void Main()
         {
-            Thread thread2 = new Thread(DisplayNumberOfRequest);
+            var thread2 = new Thread(DisplayNumberOfRequest);
             thread2.Start();
 
-            TestType test_type = TestType.ValueType;
+            const TestType TestType = TestType.ValueType;
 
-            switch (test_type)
+            switch (TestType)
             {
+#pragma warning disable 162
+// ReSharper disable HeuristicUnreachableCode
                 case TestType.ValueType:
                     {
-                        for (int i = 0; i < Program.NbThread; i++)
+                        for (int i = 0; i < NbThread; i++)
                         {
-                            Thread thread = new Thread(CallTest);
+                            var thread = new Thread(CallTest);
                             thread.Start(i);
                         }
                     }
@@ -224,9 +240,9 @@ namespace LoadTester
                     break;
                 case TestType.ScriptingDictionary:
                     {
-                        for (int i = 0; i < Program.NbThread; i++)
+                        for (int i = 0; i < NbThread; i++)
                         {
-                            Thread thread = new Thread(CallTestScriptingDictionary);
+                            var thread = new Thread(CallTestScriptingDictionary);
                             thread.Start(i);
                         }
                     }
@@ -234,18 +250,18 @@ namespace LoadTester
                     break;
                 case TestType.ValueTypeAndIisSession:
                     {
-                        for (int i = 0; i < Program.NbThread; i++)
+                        for (int i = 0; i < NbThread; i++)
                         {
-                            Thread thread = new Thread(CallTest);
+                            var thread = new Thread(CallTest);
                             thread.Start(i);
                         }
 
-                        Thread thread3 = new Thread(DisplayNumberOfRequestOldSess);
+                        var thread3 = new Thread(DisplayNumberOfRequestOldSess);
                         thread3.Start();
 
-                        for (int i = 0; i < Program.NbThread; i++)
+                        for (int i = 0; i < NbThread; i++)
                         {
-                            Thread thread4 = new Thread(CallTestIisSession);
+                            var thread4 = new Thread(CallTestIisSession);
                             thread4.Start(i);
                         }
                     }
@@ -255,6 +271,8 @@ namespace LoadTester
                     {
                         throw new Exception("This should not happend");
                     }
+// ReSharper restore HeuristicUnreachableCode
+#pragma warning restore 162
             }
 
             Console.ReadLine();
